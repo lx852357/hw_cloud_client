956d72be5ac147dcac8badbe5a5cf35f3d7254cb
lx852357 <lx852357@outlook.com>
2019-08-09 14:01:06 +0800

    update message.h

--- include/message.h
+++ include/message.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef _MESSAGE_H_
 #define _MESSAGE_H_
 
@@ -5,6 +6,37 @@
 #include "cJSON.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <iostream>
+
+struct Point {
+	int x;
+	int y;
+};
+
+struct TeamInfo {
+	int id;
+	int players[4];
+	char* force;
+};
+
+struct WormholePair {
+	int name1;
+	int name2;
+	Point point1;
+	Point point2;
+};
+
+struct Tunnel {
+	Point point;
+	int direct;//上下左右 20 21 22 23
+};
+
+struct Power {
+	Point point;
+	int value;
+};
+
 
 enum DIRECT
 {
@@ -12,7 +44,11 @@ enum DIRECT
 	MOVE_DOWN = 1,
 	MOVE_LEFT = 2,
 	MOVE_RIGHT = 3,
-	NO_MOVE = 4
+	NO_MOVE = 4,
+	UP = 20,
+	DOWN = 21,
+	LEFT = 22,
+	RIGHT = 23
 };
 
 struct SubAction
@@ -43,16 +79,24 @@ public:
     ~LegStartMsg(){cJSON_Delete(root);};
 
     void DecodeMessge(int& myTeamId,int myPlayerId[4]);
-    void GetMyTeamInfo(int& myTeamId,int myPlayerId[4]);
-    void GetMeteor(int& myTeamId);
-    void GetWormhole(int& myTeamId);
-    void GetTunnel(int& myTeamId);
-    void GetTeams(int& myTeamId);
-    void GetCloud(int& myTeamId);
+    void GetMyTeamInfo(int& myTeamId,int myPlayerId[4]); //更新了mTeamInfo信息
+    void GetMeteor(int& myTeamId);                       //更新了mMeteors信息
+    void GetWormhole(int& myTeamId);                     //更新了mWormholes信息
+    void GetTunnel(int& myTeamId);                       //更新了mTunnels信息
+    void GetCloud(int& myTeamId);                        //协议里应该是没有这个的
+	void GenerateMap();                                  //当所有信息都生成后，用这个生成map信息
 private:
 
 public:
+	char* DIRECTLOG[4] = { "UP", "DOWN", "LEFT", "RIGHT" };
+
     cJSON* root;
+	int h;
+	int w;
+	TeamInfo mTeamInfo;
+	std::vector<Point> mMeteors;
+	std::vector<WormholePair> mWormholePairs;
+	std::vector<Tunnel> mTunnels;
 };
 
 class RoundMsg
@@ -91,6 +135,8 @@ public:
 public:
     cJSON* root;
 };
+
+/// 
 
 #endif
 
